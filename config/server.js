136978dff58fc6e9ab78228d9cea0c9df55d7c6b
MeyerFrenchel <meyer.frenchel@gmail.com>
2021-01-19 10:33:36 +0200

    done

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5c6b00a9929b8e67354e185da71c95dc'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '61b9a852-ff1a-43ba-8a80-4c21648aa363'
+};

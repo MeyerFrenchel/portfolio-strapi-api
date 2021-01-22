adb2dc7cbe96457553119dc77075b1a30310e726
MeyerFrenchel <meyer.frenchel@gmail.com>
2021-01-22 16:49:28 +0200

    retry cloudinary instalation2

--- config/plugins.js
+++ config/plugins.js
@@ -3,9 +3,9 @@ module.exports = ({ env }) => ({
     upload: {
       provider: 'cloudinary',
       providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
+        cloud_name: env('meyercloud'),
+        api_key: env('345668496829211'),
+        api_secret: env('SqWG4svVQkxf09cincnTEa2He6Q'),
       },
     },
     // ...
